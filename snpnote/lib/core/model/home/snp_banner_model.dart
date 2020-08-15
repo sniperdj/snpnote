d8703e324a2adee0e911b6e199751170ae26a6de
Sniper <sniper_zheng@163.com>
2020-08-15 15:18:28 +0800

    temp

--- snpnote/lib/core/model/home/snp_banner_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class SNPBannerModel {
-  String id;
-  String url;
-  String desc;
-
-  SNPBannerModel({this.id, this.url, this.desc});
-}
--- snpnote/lib/core/viewmodel/home/snp_home_view_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:snpnote/ui/pages/home/mock_home.dart';
-
-class SNPHomeViewModel extends ChangeNotifier {
-  // 获取首页cell中的mock数据
-  static getMockData() async {
-    return Future.value(mockHomeLists);
-  }
-}
